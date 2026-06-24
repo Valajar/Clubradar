67a0b6a3fd23113b8b0b40273609bc8b4743e858
milo <milo.lp.ds@gmail.com>
2026-06-24 15:47:25 +0200

    Linker Bereich neben der Karte mit einer Übersicht ergänzt, alle Labels in die Mitte verschoben

---
+++ swingRadar/src/Login/forms/ClubOverview.java
@@ -0,0 +1,14 @@
+package Login.forms;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClubOverview extends JPanel {
+    private JPanel mainPanel;
+    private JLabel Ueberschrift;
+    private JLabel Club1;
+
+    public ClubOverview() {
+        add(mainPanel, BorderLayout.CENTER);
+    }
+}
